5f64d4512999f6c0f34fd8b42bee51592bf7ea0e
Alapan Mukherjee <alapan.mukherjee35@gmail.com>
2025-02-01 22:03:55 +0000

    Create spinning wheel using Canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef } from 'react';
+import styles from './App.module.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const ref = useRef<HTMLCanvasElement>(null);
+  const segments = [
+    'Sherlock Holmes',
+    'Hercule Poirot',
+    'Miss Marple',
+    'Feluda',
+    'Byomkesh Bakshi',
+    'Philip Marlowe',
+  ];
+
+  const colors = [
+    '#f8c471',
+    '#3498db',
+    '#a9dfbf',
+    '#5b2c6f',
+    '#5f6a6a',
+    '#73c6b6',
+  ];
+
+  useEffect(() => {
+    const canvas = ref.current;
+    if(!canvas) return;
+
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return;
+
+    const wheelRadius = canvas.width / 2;
+    const anglePerSegment = (2 * Math.PI) / segments.length;
+    const currentAngle = 0;
+
+    for(let i = 0; i < segments.length; i++) {
+      const startAngle = currentAngle + i * anglePerSegment;
+      const endAngle = startAngle + anglePerSegment;
+      ctx.beginPath();
+      ctx.moveTo(wheelRadius, wheelRadius);
+      ctx.arc(wheelRadius, wheelRadius, wheelRadius, startAngle, endAngle);
+      ctx.fillStyle = colors[i];
+      ctx.fill();
+      ctx.stroke();
+      ctx.closePath();
+
+      ctx.save();
+      ctx.translate(wheelRadius, wheelRadius);
+      ctx.rotate(startAngle + anglePerSegment / 2);
+      ctx.textAlign = 'right';
+      ctx.fillStyle = 'black';
+      ctx.font = '20px Arial';
+      ctx.fillText(segments[i], wheelRadius - 10, 0);
+      ctx.restore();
+    }
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className={styles.container}>
+      <canvas
+        ref={ref}
+        width={500}
+        height={500}
+      />
+    </div>
   )
 }
 
